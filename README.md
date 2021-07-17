8c4fbc8b3fcdbd9d7dc564712c086e48d7b96fae
众触应用平台 <87555332+zclc@users.noreply.github.com>
2021-07-17 21:07:36 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ctrl-S事件
+
+众触零码应用平台插件
+
+Plugins For ZhongChu ZeroCode Application Platform
---
+++ plugins/zp132_Ctrl-S事件.js
@@ -0,0 +1,27 @@
+function save(e) {
+    if (e.keyCode === 83 && e.ctrlKey && e.keyCode !== 17) {
+        e.preventDefault()
+        const ref = window.zp132
+        ref.exc(ref.props.exp || "$el.previousElementSibling.click()", { ...ref.ctx, $el: ref.container, $event: e })
+    }
+}
+
+function onInit(ref) {
+    window.zp132 = ref
+    document.addEventListener("keydown", save)
+}
+
+function onDestroy(ref) {
+    document.removeEventListener("keydown", save)
+}
+
+$plugin({
+    id: "zp132",
+    props: [{
+        prop: "exp",
+        type: "exp",
+        label: "事件表达式"
+    }],
+    onInit,
+    onDestroy
+})
